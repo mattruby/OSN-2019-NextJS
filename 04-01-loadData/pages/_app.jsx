9b878a701ca50a21675ea605e5ab9a97df850e42
Matt Ruby <matt.ruby@bluestembrands.com>
2019-05-16 16:44:21 -0500

    upgrade all deps

--- 04-01-loadData/pages/_app.jsx
+++ 04-01-loadData/pages/_app.jsx
@@ -5,6 +5,15 @@ class MyApp extends App {
   static async getInitialProps({ Component, ctx }) {
     let pageProps = {};
 
+    /** ctx
+     * @param {String} pathname path of the URL
+     * @param {Object} query GET params parsed as an object
+     * @param {String} asPath full path as shown in browser
+     * @param {Object} req HTTP request (server only)
+     * @param {Object} res HTTP response (server only)
+     * @param {Object} error Render error if any
+     */
+
     if (Component.getInitialProps) {
       pageProps = await Component.getInitialProps(ctx);
     }
