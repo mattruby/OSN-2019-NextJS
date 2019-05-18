333b70bd52868c97cf5ba108df98c421e53c3ace
Matt Ruby <matt.ruby@bluestembrands.com>
2019-05-17 22:17:29 -0500

    Enhanced the search form to work without JS

--- 04-03-loadData-slug-ssr/pages/index.js
+++ 04-03-loadData-slug-ssr/pages/index.js
@@ -10,6 +10,8 @@ export default () => {
       <h1>Welcome to my awesome homepage</h1>
       <p>We sell many interesting cool products. Please buy one today!</p>
       <form
+        action="/search"
+        method="get"
         onSubmit={e => {
           e.preventDefault();
           Router.pushRoute("search", { keyword: searchTerm });
